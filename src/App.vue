ddbd1c116c2dbed2faf294ba9153c131f0285dea
Stasa67 <89081437924ni@gmail.com>
2023-04-25 23:46:26 +0300

    Страница итогов

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="result">
+  <BankComponent v-for="(item, index) in bank" :items="item.items" :img="item.img" :beaver="item.beaver" :key="index" />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BankComponent from './components/Bank.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BankComponent
+  },
+  computed: {
+    bank: function () {
+      return [{
+        img: "https://i.pinimg.com/736x/e2/ed/76/e2ed7679ae3d8421fe97def95ffde02e.jpg",
+        beaver: true ,
+        items: this.items1
+      },{
+        img: "https://static-cse.canva.com/blob/584144/.png",
+        beaver: false,
+        items: this.items2
+      },{
+        img: "https://wl-adme.cf.tsp.li/resize/728x/jpg/8c7/9eb/0a12375a07b5df30df2c696af2.jpg",
+        beaver: true,
+        items: this.items3
+      }
+    ]
+    },
+    items1: function () {
+      return [
+      {
+        title: "прозрачные условия"
+      },{
+        title: "входит в АВС"
+      },
+      {
+        title: "есть лицензия Банка РФ"
+      },
+      {
+        title: "честный договор"
+      }
+    ]
+    },
+    items2: function () {
+      return [
+      {
+        title: "непрозрачные условия"
+      },{
+        title: "входит в АВС"
+      },
+      {
+        title: "есть лицензия Банка РФ"
+      },
+      {
+        title: "фактический процент меньше"
+      }
+    ]
+    },
+    items3: function () {
+      return [
+      {
+        title: "самые выгодные условия"
+      },{
+        title: "лицензия отозвана"
+      },
+      {
+        title: "плохой договоp"
+      },
+      {
+        title: "реклама на сайте"
+      }
+    ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Bank.vue
@@ -0,0 +1,87 @@
+<template>
+<div class="result">
+  <div class="result-avatar">
+    <img :src="img" class="result-avatar__img">
+  </div>
+  <div class="result-factor">
+    <div class="result-factor__item" v-for="item in items" :key="item.title">{{ item.title }}</div>
+  </div>
+  <div class="result-beaver" v-if="beaver">
+    <img class="result-beaver__img" src="https://img.freepik.com/premium-vector/cute-cartoon-beaver-reading-a-book_290315-1403.jpg">
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'BankComponent',
+  props: {
+    title: Storage,
+    img: String,
+    beaver: Boolean,
+    items: Array
+  }
+}
+</script>
+
+
+<style>
+.result {
+  margin-top: 20px;
+  width: 860px;
+  height: 158px;
+  background: #4e63d980;
+  border-radius: 30px;
+}
+
+.result-avatar {
+  position: absolute;
+  width: 70px;
+  height: 70px;
+  margin: 14px 0 0 20px;
+  border-radius: 100%;
+  background: #F2F2F2;
+}
+
+.result-factor {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding-left: 162px;
+  padding-right: 60px;
+}
+
+.result-factor__item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 214px;
+  height: 40px;
+  margin: 20px;
+  background: #5c6273bf;
+  border-radius: 30px;
+  color: #F2F2F2;
+}
+
+.result-avatar__img {
+  display: flex;
+  width: 70px;
+  height: 70px;
+  border-radius: 100%;
+  align-items: center;
+}
+
+.result-beaver {
+  position: absolute;
+  transform: rotate(346deg);
+  top: 46px;
+  left: 720px;
+  width: 140px;
+  height: 140px;
+}
+
+.result-beaver__img {
+  width: 140px;
+  height: 140px;
+}
+
+</style>
